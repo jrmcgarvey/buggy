06941e759185241878cffce9a81ff95eab082613
John R. McGarvey <johnmcgarvey55@gmail.com>
2024-07-02 14:49:24 -0400

    correct security holes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,14 @@
 class UsersController < ApplicationController
   include ActionController::Cookies
-  before_action :user_logged_in, only: %i[setphrase getuser]
+  before_action :user_logged_in?, only: %i[setphrase getuser]
+  before_action :validate_csrf, only: :setphrase
 
   def register
-    salt = Rails.application.credentials.password_salt
-    u_params = user_params
-    u_params[:password_digest] = BCrypt::Engine.hash_secret(u_params[:password], salt)
-    u_params.delete(:password)
-    user = User.create(u_params)
+    # salt = Rails.application.credentials.password_salt
+    # u_params = user_params
+    # u_params[:password_digest] = BCrypt::Engine.hash_secret(u_params[:password], salt)
+    # u_params.delete(:password)
+    user = User.create(user_params)
     if user.errors.empty?
       render json: { message: "A user record for #{user_params['name']} was created." }, status: 201
     else
@@ -17,21 +18,36 @@ class UsersController < ApplicationController
   end
 
   def logon
-    salt = Rails.application.credentials.password_salt
-    password_hash = BCrypt::Engine.hash_secret(logon_params[:password], salt)
-    users = User.where("email = '#{logon_params[:email]}' AND password_digest = '#{password_hash}'")
-    if users.empty?
+    # salt = Rails.application.credentials.password_salt
+    # password_hash = BCrypt::Engine.hash_secret(logon_params[:password], salt)
+    # users = User.where("email = '#{logon_params[:email]}' AND password_digest = '#{password_hash}'")
+    #
+    # One could do User.where(email: logon.params[:email], password_digest: password_hash))
+    # That would get rid of the SQL injection, but does not work when each password is hashed
+    # with a different salt.
+    #
+    # if users.empty?
+    #   render json: { message: 'Authentication failed.' }, status: 401
+    begin
+      @user = User.find_by_email(logon_params[:email].downcase)
+    rescue ActiveRecord::RecordNotFound
+      @user = nil
+    end
+    if !@user | !@user.authenticate(logon_params[:password])
       render json: { message: 'Authentication failed.' }, status: 401
     else
-      @user = users[0]
+      # @user = users[0]
       hmac_secret = Rails.application.credentials.secret_key_base
-      payload = { id: @user.id }
+      csrf_token = SecureRandom.uuid
+      payload = { id: @user.id, csrf_token: }
       token = JWT.encode payload, hmac_secret, 'HS256'
       # ignore the following, as it is a workaround for a Postman problem.
       secure_cookie = (ENV['INSECURE_COOKIES'] != 'true')
       # end of Postman workaround
       cookies['JWT'] =
         { value: token, same_site: :None, secure: secure_cookie, partitioned: true, httponly: true }
+      cookies['CSRF_TOKEN'] = { value: csrf_token, same_site: :Strict, secure: secure_cookie }
+      @user.reload
       @user.phrase = '' unless @user.phrase
       render json: { user: { name: @user.name, phrase: @user.phrase } }, status: 201
     end
@@ -68,24 +84,27 @@ class UsersController < ApplicationController
   end
 
   def user_logged_in?
-    if !cookies['JWT']
-      render json: { message: 'No user is authenticated' }, status: 401
+    return render json: { message: 'No user is authenticated' }, status: 401 unless cookies['JWT']
+
+    hmac_secret = Rails.application.credentials.secret_key_base
+    begin
+      decoded_token = JWT.decode cookies['JWT'], hmac_secret, { algorithm: 'HS256' }
+    rescue StandardError
+      render json: { message: 'Invalid JWT provided.' }, status: 401
       return false
-    else
-      hmac_secret = Rails.application.credentials.secret_key_base
-      begin
-        decoded_token = JWT.decode cookies['JWT'], hmac_secret, { algorithm: 'HS256' }
-      rescue StandardError
-        render json: { message: 'Invalid JWT provided.' }, status: 401
-        return false
-      end
-      puts(decoded_token)
-      @user = User.find(decoded_token[0]['id'].to_s)
-      unless @user
-        render json: { message: 'Invalid user specified.' }, status: 401
-        return false
-      end
     end
-    true
+    puts(decoded_token)
+    begin
+      @user = User.find_by_id(decoded_token[0]['id'])
+    rescue ActiveRecord::RecordNotFound
+      return render json: { message: 'Invalid user specified.' }, status: 401
+    end
+    @csrf_token = decoded_token[0]['csrf_token']
+  end
+
+  def validate_csrf
+    return unless @csrf_token != request.headers['X-CSRF-TOKEN']
+
+    render json: { message: 'CSRF token validation failed.' }
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ApplicationRecord
-    validates :name, presence: true
-    validates :email, presence: true, uniqueness: true
+    has_secure_password
+    validates :name, presence: true, format: { with: 
+    /\A[a-zA-Z]+(([\'\,\.\- ][a-zA-Z ])?[a-zA-Z]*)*\z/ , message: "Please enter a valid name" }
+    validates :email, presence: true, uniqueness: true, format: { 
+        with: /\A[A-Za-z0-9+_.-]+@([A-Za-z0-9]+\.)+[A-Za-z]{2,6}\z/ , message: "Please enter a valid email address"}
+    validates :password, password_strength: true
+    before_save :phrase_escape
+
+    private
+
+    def phrase_escape
+        self.phrase = Rack::Utils.escape_html(phrase)
+        self.email = email.downcase
+    end
 end
